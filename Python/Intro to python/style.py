2c4c1213bcff683558753b8d5e18af6205af4841
Jesus  Mendoza <115962951+ChuyMendoza333@users.noreply.github.com>
2023-05-01 09:12:28 -0600

    Create style.py

---
+++ Python/Intro to python/style.py
@@ -0,0 +1,50 @@
+%%html
+<style>
+p {text-align: justify !important;
+    #color: OldLace;
+  line-height: 129%;}
+b { text-align: justify;
+    color: Cyan;} 
+strong { color: LightGreen;
+       }
+mark { background-color: gainsboro;
+  color: crimson;
+  padding: 0 0.05em;
+  border: 1px solid #orange ;
+  border-radius: 2px;
+  font-weight: bold;}   
+ul {text-align: justify !important;
+    color: LightSkyBlue;}    
+i {text-align: justify !important;
+    color: DeepPink !important;}
+em {    text-align: justify;
+    color: Yellow;}
+dl {text-align: center !important;
+    background-color: lawngreen;#LightGreen;
+    padding: 1.5em 1.5em;
+    color: black;
+    font-weight: bold;}   
+href { text-aligne: center;
+    color: yellow;
+    font-weight: bold;}
+h1 { text-align: center !important;
+    color: orange;
+ font-size: 40px !important;
+ font-weight: bold !important;}
+h2 {text-align: center !important;
+     font-size: 36px !important;
+     font-weight: bold !important;}
+h3 { text-align: center !important;
+    font-size: 32px !important;
+ font-weight: bold !important;}
+h4 { text-align: left !important;
+   font-size: 28px !important;}
+
+hr { border-top: 7px dotted DarkOrange; !important;
+ #border: 2px solid Fuchsia;
+ border-radius: 6px;}
+div {
+    color: Fuchsia;
+    font-weight: bold; !important;
+    }
+</style>
